4bee4d0af0583f98b021284bdecfa4ba02cdd624
abhizp <138595328+abhizp@users.noreply.github.com>
2024-07-31 14:30:17 +0000

    new file

---
+++ .devcontainer/01bascis/variables.js
@@ -0,0 +1,8 @@
+const accoundId = 144553
+let accountEmail = "abhi@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+// accoundId = 2
+console.log();
+console.table([accoundId, accountEmail, accountPassword,accountCity])
