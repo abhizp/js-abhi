1a1d5fbb1fb63c238e462462fbfa4906530b3782
abhizp <138595328+abhizp@users.noreply.github.com>
2024-08-01 13:15:43 +0000

    good job

---
+++ .devcontainer/01bascis/numbers.js
@@ -0,0 +1,29 @@
+// const num = 100
+
+// console.log(num.toFixed(4)); // 100.0000 // returns number
+
+// const newNum = new Number(300)
+// console.log(newNum);  // [Number: 300]
+
+// console.log(num + newNum); 400
+
+// const numAgain = 23.11                 // 23.999 => 24.9 gives the nearest precise values based on the number specified the length of the number
+// console.log(numAgain.toPrecision(3)); // returns the string
+
+// ++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++
+// console.log(Math); // Object [Math] {}
+
+// console.log(Math.abs(-20)); //-ve to +ve
+// console.log(Math.round(11.1)); // gives integer without point => 11
+// console.log(Math.pow(10,5)); // (number, power)
+// console.log(Math.min(1,-33,5,7)); // -33 gives the min num
+
+// console.log(Math.random());  // value is always between 0 and 1
+// console.log(Math.random()*10);  // value is always between 0 and 10
+// console.log(Math.floor((Math.random()*10)) + 1);  // value is always between 1 and 10 , no more decimals
+
+// const min = 0
+// const max = 9999
+
+// console.log((Math.floor(Math.random()*(max - min+1)) + min));
+
