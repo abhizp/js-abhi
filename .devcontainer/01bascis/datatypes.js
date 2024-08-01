daf33315bc3eafdfa9b869f8e2f9099a6d3f7688
abhizp <138595328+abhizp@users.noreply.github.com>
2024-08-01 12:20:23 +0000

    doing good

--- .devcontainer/01bascis/datatypes.js
+++ .devcontainer/01bascis/datatypes.js
@@ -1,4 +1,4 @@
-"use strict"; // treat all js code as newer version
+ // treat all js code as newer version
 
 // alert(3+3) // we are using node.js not browser so it doesnt work
 let a = -1
---
+++ .devcontainer/01bascis/strings.js
@@ -0,0 +1,26 @@
+const name = "Abhi"
+const repNum = 50
+// console.log(name + repNum);
+// console.log(`My name is ${name} and my age would be ${repNum}`);
+
+const nameNew = new String("Abhishek")
+// console.log(nameNew.__proto__);
+// console.log(nameNew.toUpperCase) // [Function: toUpperCase]
+// const nan = nameNew.toUpperCase
+// console.log(nan);  // [Function: toUpperCase]
+// console.log(nameNew.toUpperCase())  // ABHISHEK  // method () => is expected at the end
+// console.log(nameNew.length); // property () => not allowed
+// console.log(nameNew.charAt(3)); // i
+// console.log(nameNew.indexOf("A")); // a != A in js // if element not found returns -1
+// const newString = nameNew.substring(-1,0) // (first, last) // no negative indexes allowed, ignores negative and starts from zero itself
+// console.log(newString);
+
+// const anotherString = nameNew.slice(-8,-1) // (start, end) // negative index azllowed by its -8.....-1 yeah
+// console.log(anotherString);
+
+// const newStr = "   Abhis   "  // removes all the white spaces
+// console.log(newStr.trim())
+// const url = "https://abhishek.com/Abhishek%20patil"
+// console.log(url.replace("%20","-"));
+// console.log(url.includes("abhi")); // true
+// console.log(url.split("",6)); // gives array with each element seperately "6" tells us the limit if arrray 
