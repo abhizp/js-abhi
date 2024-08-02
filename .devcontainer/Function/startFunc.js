4fbdae3d7987942c4f67c35dcbf765112b484782
abhizp <138595328+abhizp@users.noreply.github.com>
2024-08-02 19:08:00 +0000

    good nyt

---
+++ .devcontainer/Function/startFunc.js
@@ -0,0 +1,33 @@
+// function abhi(){
+//     const name = " abhi"
+//     function nandu(){
+//         const name2 = "Nandu"
+//         console.log(name, name2);        
+//     }
+//     nandu()
+//     console.log(name);  
+// }
+// abhi()   // this is the only crct nested way
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// const abhi = () => {
+//     return "abhi"
+// }
+// console.log(abhi()); // arrow function
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// (function abhi(num1){
+//     console.log("nandu" + num1);    
+// })(2)  // IIFE immediately invoked function expressions, named iifi
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// (()=>{
+//     console.log("Abhi");  
+// })() // unnamed IIFI
